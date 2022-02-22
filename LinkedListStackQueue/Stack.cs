aeca3a322ac27b90c8b6ca73c6d6d2f85a10c8e4
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2022-02-22 12:56:48 +0100

    optimized

--- LinkedListStackQueue/Stack.cs
+++ LinkedListStackQueue/Stack.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace LinkedListStackQueue
 {
-    internal class Stack
+    internal class Stack<T>
     {
+        private Snode<T> data;
         public void Push()
         {
 
